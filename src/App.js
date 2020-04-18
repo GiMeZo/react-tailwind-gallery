5fa9f5c3928888915c0e8abb75cb7afe74e13bb2
GiMeZo <salah@marocplex.com>
2020-04-18 00:17:38 +0000

    Add Project Files

---
+++ src/App.js
@@ -0,0 +1,12 @@
+import React from 'react'
+
+const App = () => {
+  return (
+    <div className="font-body container px-4 uppercase text-center">
+      <h1 className="text-5xl">Hello React</h1>
+      <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Quo ex dignissimos ipsa temporibus nostrum. Dolor accusamus provident adipisci, aliquid laudantium nostrum inventore blanditiis earum necessitatibus corrupti et ab! Soluta, cum.</p>
+    </div>
+  )
+}
+
+export default App
---
+++ tailwind.config.js
@@ -0,0 +1,13 @@
+module.exports = {
+  theme: {
+    container: {
+      center: true,
+    },
+    fontFamily: {
+      'body': ['Dosis', 'sans-serif']
+    },
+    extend: {},
+  },
+  variants: {},
+  plugins: [],
+}
